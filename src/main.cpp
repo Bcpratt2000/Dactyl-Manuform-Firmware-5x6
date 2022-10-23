4a14468a5fa2ef850d0d1a59d262257675245ba1
bcpratt2000 <bcpratt2000@gmail.com>
2022-10-22 19:45:25 -0600

    added most of the keymap

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,11 @@
 
 USBKeyboard keyboard;
 enum ADDITIONAL_KEY{
-  KEY_ESC = 27
+  KEY_ESC = 27,
+  KEY_TAB = 9,
+  KEY_BACKSPACE = 8,
+  KEY_ENTER = 13,
+  KEY_SPACE = 23
 };
 #define LED 25
 #define SLEW_US 10  // time is microseconds to wait between setting the pin
@@ -25,19 +29,19 @@ bool lastState[MATRIX_WIDTH][MATRIX_HEIGHT];
 bool currentState[MATRIX_WIDTH][MATRIX_HEIGHT];
 
 // define the pins used for the key matrix
-short outputPins[MATRIX_HEIGHT] = {0, 1, 2, 3, 4, 5};
-short inputPins[MATRIX_WIDTH] = {6, 7, 8, 9, 10, 11};
+short outputPins[MATRIX_HEIGHT] = {2, 3, 4, 5, 6, 7};
+short inputPins[MATRIX_WIDTH] = {8, 9, 10, 11, 12, 13};
 
 // Define Keymaps
 #ifdef LEFT
 short Keymap[MATRIX_WIDTH][MATRIX_HEIGHT] = {
-    {KEY_ESC, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
+    {KEY_ESC, '1', '2', '3', '4', '5'}, {KEY_TAB, 'q', 'w', 'e', 'r', 't'}, {KEY_SHIFT, 'a', 's', 'd', 'f', 'g'},
+    {KEY_CTRL, 'z', 'x', 'c', 'v', 'b'}, {'[', ']', KEY_ENTER, KEY_SPACE, 0, 0}, {0, 0, 0, 0, 0, 0}};
 #endif
 #ifdef RIGHT
 short Keymap[MATRIX_WIDTH][MATRIX_HEIGHT] = {
-    {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
+    {'6', '7', '8', '9', '0', KEY_BACKSPACE}, {'y', 'u', 'i', 'o', 'p', '-'}, {'h', 'j', 'k', 'l', ';', '\''},
+    {'n', 'm', ',', '.', '/', '\\'}, {0, 0, KEY_SPACE, KEY_ENTER, '=', '-'}, {0, 0, 0, 0, 0, 0}};
 #endif
 
 void setup() {
