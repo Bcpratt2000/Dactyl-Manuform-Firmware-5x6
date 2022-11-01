2c65ae47248093a7386a9378cd0d76f93300f334
bcpratt2000 <bcpratt2000@gmail.com>
2022-11-01 09:46:07 -0600

    added i2c file

---
+++ src/I2CKeyboard.cpp
@@ -0,0 +1,22 @@
+#include "I2CKeyboard.h"
+
+#define MAX_KEYS 6  // maximum number of keys that can be pressed at once
+
+I2CKeyboard::I2CKeyboard(unsigned int bitrate){
+  // Wire.begin(bitrate);
+}
+
+void I2CKeyboard::sendKeys(uint8_t modifier, uint8_t* keys) {
+  HID_REPORT report;
+  report.data[0] = 1;
+  report.data[1] = modifier;
+  report.data[2] = 0;
+
+  for (int i = 0; i < MAX_KEYS; i++) {
+    report.data[3 + i] = keys[i];
+  }
+
+  report.length = 3 + MAX_KEYS;
+
+  keyboard.send(&report);
+}
---
+++ src/I2CKeyboard.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Arduino.h>
+#include <PluggableUSBHID.h>
+#include <USBHID_Types.h>
+#include <USBKeyboard.h>
+#include <Wire.h>
+
+#include "KEYS.h"
+
+#define MAX_KEYS 6 //maximum number of keys that can be pressed at once
+
+class I2CKeyboard {
+ public:
+  I2CKeyboard(unsigned int bitrate);
+  void sendKeys(uint8_t modifier, uint8_t* keys);
+
+
+ private:
+  USBKeyboard keyboard;
+};
